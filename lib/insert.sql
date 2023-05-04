fb9d6e510c37eb0489fbaf96de1297880c0e3e83
Yinson Tso <yinson.tso@gmail.com>
2023-05-04 16:07:55 -0400

    yes

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, sex)
+VALUES ("Mr. Chocolate", "M");
+
+INSERT INTO bears (name, sex)
+VALUES ("Rowdy", "M");
+
+INSERT INTO bears (name, sex)
+VALUES ("Tabitha", "F");
+
+INSERT INTO bears (name, sex)
+VALUES ("Sergeant Brow", "M");
+
+INSERT INTO bears (name, sex)
+VALUES ("Melissa", "F");
+
+INSERT INTO bears (name, sex)
+VALUES ("Grinch", "M");
+
+INSERT INTO bears (name, sex)
+VALUES ("Wendy", "F");
+
+INSERT INTO bears (name, sex)
+VALUES (?, ?);
